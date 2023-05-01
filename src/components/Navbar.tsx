3a016ff5d001048b5298258fe2d44cb3e5b77062
james-kwok <kinkwan.j@gmail.com>
2023-05-01 14:35:14 -0400

    header wip

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/navLogo.png"
+          alt="logo"
+          width="25"
+          height="25"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
